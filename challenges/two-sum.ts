fd5bb83e315b7a0e20b80d85716ed14702cbef0c
Katy Wells <katy.wells@verizon.net>
2026-03-09 09:25:51 -0400

    merge new probs

--- challenges/two-sum.ts
+++ challenges/two-sum.ts
@@ -16,10 +16,19 @@
   Rationale: No elements inside the array sum up to the target number
 */
 
+// if I assume array is sorted, start with first el, then only check against els whose val is <= diff
+// if not sorted, (what time/space to sort?)
 export const twoSum = (arr: number[], target: number): boolean => {
+  for (let i = 0; i < arr.length; i++) { //i=2
+    let diff = target - arr[i]; //diff=13
+    for (let j = 1; j < arr.length; j++) {//j=5
+        if (arr[i] + arr[j] === target) return true;
+    }
+  }
   return false;
 };
-
+const nums = [2, 5, 10, 45]
+console.log(twoSum(nums, 35)) //-> true
 /*
 Extension:
 Given an array of numbers and a target number, 
