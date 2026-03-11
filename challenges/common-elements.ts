7f2bc011fea7c6172522520046445689d4d29bb4
Katy Wells <katy.wells@verizon.net>
2026-03-11 10:01:51 -0400

    practice hackhour challenges

--- challenges/common-elements.ts
+++ challenges/common-elements.ts
@@ -16,12 +16,51 @@ commonElements(arr1, arr2, arr3) -> [2, 3, 2000, 'dog']
 After completing your initial approach, try refactoring your solution so that it completes in O(n) time.
 
 */
-
+//input: n * arrays of num || strings
+//output: 1 array of shared els from input arrays || "Nothing in Common!"
+/*
+How do I check how many input arrays there are?
+* check args.length for total num of arrays
+* reduce first array 
+*   if acc[currVal] exists, increment its value by 1, else acc[currVal] = 1 (create obj with # of times each el occurs)
+* 
+* NO - two pointers, 
+*/
 const commonElements = (
   ...args: (string | number)[][]
 ): (string | number)[] | 'Nothing in Common!' => {
+  const totalArrs = args.length;
+  console.log(totalArrs);
+  const firstArr = args[0];
+  console.log(firstArr);
+  // const resultArr = [];
+  // // edge - if there are no input arrays or only one
+  // if (!totalArrs || totalArrs <= 1) return 'Nothing in Common!';
+  // for (let i = 0; i < totalArrs; i++) {
+  //   const reducedArrs = args[i].reduce((acc, curr) => {
+  //     console.log(acc[curr]);
+  //     if (acc[curr]) {
+  //       acc[curr] = acc[curr] + 1;
+  //     }
+  //     acc[curr] = 1;
+  //   });
+  //   for (const key in reducedArrs) {
+  //     if (reducedArr[key] === totalArrs) {
+  //       resultArr.push(key);
+  //     }
+  //   }
+  // }
+  // if (resultArr.length === 0) return 'Nothing in Common!';
+  // return resultArr;
   return [];
 };
+
+const arr1 = [2, 10,'cat', 3, 99, 2000, 'dog', 'lion'];
+const arr2 = [3, 7, 2, 2000, 1, 'dog', 'cat'];
+const arr3 = [2, 100, 2000, 'dog', 3, 'lion'];
+
+console.log(commonElements(arr1, arr2, arr3)); // -> [2, 3, 2000, 'dog'];
+
 
 /*
 ** Extension **
