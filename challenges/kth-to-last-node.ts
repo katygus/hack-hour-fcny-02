a5e979a6bb73c5a1562bf0f6e75db4f1a1f064f6
Katy Wells <katy.wells@verizon.net>
2026-03-30 10:38:51 -0400

    finish kth to the last node

--- challenges/kth-to-last-node.ts
+++ challenges/kth-to-last-node.ts
@@ -21,7 +21,7 @@
  * - When k is 1, the function should return the tail node.
  * - When k is the length/size of the linked list, the function should return the head node.
  */
-
+// edge cases: input is 0, input is longer than total length of list (both undefined)
 class Node<T> {
   value: T;
   next: Node<T> | null;
@@ -31,10 +31,47 @@ class Node<T> {
     this.next = null;
   }
 }
-
+// to create map, 
 const kthToLastNode = <T>(k: number, head: Node<T>): T | undefined => {
+  if (k === 0) return undefined;
+  let slow = head;
+  let fast = head;
+  let steps = 0;
+
+// move fast pointer to k distance from slow
+ for (let i = 1; i < k; i++) {
+  fast = fast.next!;
+
+  if (fast === null) return undefined;
+  }
+  while (fast.next !== null) {
+    fast = fast.next;
+    slow = slow.next!;
+  }
+  return slow.value;
+
+  // const mappedList = new Map();
+
+  // while (curr.next !== null); {
+  // mappedList.set('curr.value', count);
+  // count++;
+  // curr = curr.next!;
+  // }
+  // console.log(mappedList);
   return undefined;
 };
 
+const a = new Node('A');
+    const b = new Node('B');
+    const c = new Node('C');
+    const d = new Node('D');
+    const e = new Node('E');
+
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    d.next = e;
+
+console.log(kthToLastNode(3, a));
 // Don't delete the line below!
 export { Node, kthToLastNode };
